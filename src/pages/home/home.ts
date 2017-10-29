fe2c8a84e5276f5936773603d2b3082471bda0fb
Theophilus Omorebee <theo4u@ymail.com>
2017-10-29 14:25:50 +0100

    done with add new task ui flow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _alertCtrl: AlertController) {
 
+  }
+
+  /**
+   * This is to show alert prompt to enter new task
+   */
+  addNew() {
+    let prompt = this._alertCtrl.create({
+      title: 'New Task',
+      //  message: "Enter your new task",
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'Enter your new task'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            this.saveTask(data.task);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+  /**
+   * this is used to save new task to our task list
+   * @param title 
+   */
+  private saveTask(title: string) {
+    console.log("You just added ", title);
   }
 
 }
